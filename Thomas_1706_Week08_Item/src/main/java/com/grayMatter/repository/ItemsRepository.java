6ad3a93989cef72c7e1a6b1eaf534e0c49a73850
Thomas <itsthomastgeorge@gmail.com>
2024-09-06 22:03:27 +0530

    deleted files

--- Thomas_1706_Week08_Item/src/main/java/com/grayMatter/repository/ItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grayMatter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grayMatter.entities.Items;
-
-public interface ItemsRepository extends JpaRepository<Items, Integer> {
-
-}
